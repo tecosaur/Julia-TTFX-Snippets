39a58c754b919f6185892f90fec203adb1cfdbfb
gdalle <gdalle@users.noreply.github.com>
2025-11-16 18:28:55 +0000

    New task: Zygote, Gradient of square norm

---
+++ tasks/Z/Zygote/Gradient-Of-Square-Norm/task.jl
@@ -0,0 +1,24 @@
+# Task: Gradient Of Square Norm
+# Package: Zygote
+# Author: @gdalle
+# Attribution: Guillaume Dalle
+# Created: 2025-11-16
+# Sample timings: install in 55.9s, run in 1.321s
+
+__t1 = time()
+
+using Zygote
+
+__t2 = time()
+
+f(x) = sum(abs2, x)
+x = rand(10)
+gradient(f, x)
+
+__t3 = time()
+
+__t_using = __t2 - __t1
+__t_script = __t3 - __t2
+__t_total = __t3 - __t1
+println(stdout, "$__t_using, $__t_script, $__t_total seconds")
+
